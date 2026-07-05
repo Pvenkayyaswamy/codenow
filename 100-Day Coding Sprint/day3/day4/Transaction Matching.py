0116a55d4f2ac882959dc4ed4660ac822060f9ab
POTNURI VENKAYYA SWAMY <pvenkayya2005@gmail.com>
2026-07-05 19:04:34 +0530

    Delete 100-Day Coding Sprint/day3/day4 directory

--- 100-Day Coding Sprint/day3/day4/Transaction Matching.py
+++
@@ -1,9 +0,0 @@
-def twoSum(nums, target):
-    hist={}
-    for i,num in enumerate(nums):
-        diff=target-num
-        if diff in hist:
-            return [hist[diff] , i]
-        else:
-            hist[num]=i
-    return []
